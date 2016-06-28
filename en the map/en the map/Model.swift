fa680c56243257513c5a129d9e5a87c306f871df
Harry Merzin <hmerzin@gmail.com>
2016-06-28 14:56:25 -0400

    everything is optimal but the mapView is nil

---
+++ en the map/en the map/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  en the map
+//
+//  Created by Harry Merzin on 6/26/16.
+//  Copyright © 2016 harry. All rights reserved.
+//
+
+import Foundation
---
+++ en the map/en the map/ParseNetworking.swift
@@ -0,0 +1,9 @@
+//
+//  ParseNetworking.swift
+//  en the map
+//
+//  Created by Harry Merzin on 6/24/16.
+//  Copyright © 2016 harry. All rights reserved.
+//
+
+import Foundation
